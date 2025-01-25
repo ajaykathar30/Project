bae53ed3e5e43d1304c6ccc17e517b5d3f69fce4
Abhijeet Jain <abhijeetjain1413@gmail.com>
2025-01-25 14:12:12 +0530

    Update home-script.js
    added navigation to home by clicking on logo present in navbar

--- js/home-script.js
+++ js/home-script.js
@@ -27,3 +27,7 @@ for(ele of redirect){
         window.location.href="http://127.0.0.1:3000/html/product_details.html";
     })
 }
+let logo=document.body.querySelector(".logo");
+logo.addEventListener("click",()=>{
+    window.location.href="http://127.0.0.1:3000/html/home.html";
+})
